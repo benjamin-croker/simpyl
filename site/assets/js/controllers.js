19505b698782879a0a3276623abe0522217015da
Benjamin Croker <benjamin.croker@gmail.com>
2014-02-22 22:38:26 +1000

    some more progress with angular

--- site/assets/js/controllers.js
+++ site/assets/js/controllers.js
@@ -5,12 +5,22 @@
 var simpylApp = angular.module('simpylApp', []);
 
 simpylApp.controller('EnvListCtrl', function($scope, $http) {
+  $scope.run_init = {discription: "", environment_name: "", proc_inits: []};
+
+  $scope.selected_proc_init = {};
+
   $http.get('api/envs').success(
       function(data, status) {
           $scope.envs = data.environment_names;
       });
+
   $http.get('api/proc_inits').success(
       function(data, status) {
-          $scope.proc_inits = data.proc_inits;
+          $scope.avail_proc_inits = data.proc_inits;
       });
+
+  $scope.addProc = function() {
+    $scope.run_init.proc_inits.push($scope.selected_proc_init);
+    $scope.selected_proc_init = {};
+  };
 });
