8fb315616abf1cb7e832c4695b71aabb79e4c6ca
Benjamin Croker <benjamin.croker@gmail.com>
2014-03-12 22:24:19 +1000

    Added some PyPI files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Simpyl',
+    version='0.1dev',
+    packages=['simpyl',],
+    license=open('LICENSE.txt').read(),
+    long_description=open('README.txt').read(),
+)
