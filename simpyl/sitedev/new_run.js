a703568fd299122168853d5a34a2b8a95c8c5755
benjamin-croker <benjamin.croker@gmail.com>
2020-09-19 15:24:13 +1000

    progress on new run page'

--- simpyl/sitedev/new_run.js
+++ simpyl/sitedev/new_run.js
@@ -1,18 +1,34 @@
-const defaultInput = {selected: '', value: 0}
-
 var app = new Vue({
   el: '#vue_test',
-  created() {},
+  created() {this.getProcInits()},
 
   data: {
-    input: {...defaultInput},
-    test_array: []
+    proc_inits: {},
+    proc_working: {},
+    run_init: {
+      description: "",
+      environment_name: "default",
+      proc_inits: []
+    }
   },
 
   methods: {
-    appendInput: function() {
-      this.test_array = [...this.test_array, this.input];
-      this.input = {...defaultInput};
+    addProcToRun: function() {
+      // add an ID
+      this.proc_working.run_order = this.run_init.proc_inits.length;
+      // and convert the argument string
+      this.proc_working.arguments_str = this.proc_working.arguments.map(
+        item => item.name+":"+item.value
+      ).join(", ");
+
+      this.run_init.proc_inits = [...this.run_init.proc_inits, this.proc_working];
+
+      this.proc_working = {};
+    },
+    getProcInits: function() {
+      fetch('api/proc_inits')
+        .then(response => response.json())
+        .then(jsonData => this.proc_inits = jsonData.proc_inits)
     }
   },
 })
